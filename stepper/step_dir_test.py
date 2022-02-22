487c651203d87e400082b23888f0aad007848398
taylor123454321 <ryantaylor.nz@icloud.com>
2022-02-22 20:41:30 +1300

    created more test code. More is needed.

--- stepper/step_dir_test.py
+++ stepper/step_dir_test.py
@@ -1,6 +1,29 @@
 """Program to test step/dir stepper driver"""
 """Servo library import"""
 import pigpio
-from time import sleep
+import time
+
+"""Init for GPIO for servo/tilt"""
+print("Init GPIO start")
+servoPIN = 27  # pin 13 on RPI
+pi = pigpio.pi()
+pi.set_mode(servoPIN, pigpio.INPUT)
+print("Init GPIO finished")
+
+pi.write(servoPIN, 1)
+time.sleep(0.5)
+pi.write(servoPIN, 0)
+time.sleep(0.5)
+pi.write(servoPIN, 1)
+time.sleep(0.5)
+pi.write(servoPIN, 0)
+time.sleep(0.5)
+pi.write(servoPIN, 1)
+time.sleep(0.5)
+pi.write(servoPIN, 0)
+time.sleep(0.5)
+pi.write(servoPIN, 1)
+time.sleep(0.5)
+
 
 

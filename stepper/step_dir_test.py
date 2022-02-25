c31f54d885d73b6d184520f164392d2dd1dcd1cc
taylor123454321 <ryantaylor.nz@icloud.com>
2022-02-25 19:54:56 +1300

    test file

---
+++ stepper/step_dir_test.py
@@ -0,0 +1,26 @@
+"""Program to test step/dir stepper driver"""
+"""Servo library import"""
+import pigpio
+import time
+
+"""Init for GPIO for servo/tilt"""
+print("Init GPIO start")
+stepPIN = 27  # pin 13 on RPI
+pi = pigpio.pi()
+pi.set_mode(stepPIN, pigpio.INPUT)
+print("Init GPIO finished")
+
+
+pin_operator = 1
+for i in range(1000):
+    if pin_operator == 1:
+        pi.write(stepPIN, 1)
+        print("High")
+    elif pin_operator == -1:
+        pi.write(stepPIN, 0)
+        print("Low")
+    pin_operator = pin_operator * -1
+    time.sleep(0.01)
+
+
+

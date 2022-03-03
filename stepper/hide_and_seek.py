0314b035aa196dc5ec5ee9546239255fa79b91c7
taylor123454321 <ryantaylor.nz@icloud.com>
2022-03-03 21:37:33 +1300

    commented out code for everything but move and singles

--- stepper/hide_and_seek.py
+++ stepper/hide_and_seek.py
@@ -27,7 +27,7 @@ time.sleep(1)
 
 print("Imports finished")
 
-error_angle = 0
+"""error_angle = 0
 
 MIN_PW = 1100
 MID_PW = 1300
@@ -35,14 +35,14 @@ MAX_PW = 1500
 
 status = 0  # 0 is if no pest found, 1 if pest is found
 region_global = [0]*4  # Array for centroid of pest
-
+"""
 
 """Init for PIGPIO for servo/tilt"""
-print("Init GPIO start")
+"""print("Init GPIO start")
 servoPIN = 17  # pin 11 on RPI
 pi = pigpio.pi()
 pulsewidth = MID_PW
-pi.set_servo_pulsewidth(servoPIN, pulsewidth)
+pi.set_servo_pulsewidth(servoPIN, pulsewidth)"""
 
 """Init for PIGPIO for stepper driver"""
 stepPIN = 27  # pin 13 on RPI
@@ -146,7 +146,7 @@ def init():
     pi.write(enablePIN, 1)
     # Tilt servo
     time.sleep(time_delay)
-    print("Attempting servo init tilt")
+    """print("Attempting servo init tilt")
     pi.set_servo_pulsewidth(servoPIN, MID_PW)
     time.sleep(time_delay)
     pi.set_servo_pulsewidth(servoPIN, MIN_PW)
@@ -154,10 +154,10 @@ def init():
     pi.set_servo_pulsewidth(servoPIN, MAX_PW)
     time.sleep(time_delay)
     pi.set_servo_pulsewidth(servoPIN, MID_PW)
-    time.sleep(time_delay)
+    time.sleep(time_delay)"""
 
 
-def rotate_to_target(error):
+"""def rotate_to_target(error):
     if error <= 0:  # forward
         direction = 0
     else:
@@ -201,7 +201,7 @@ def tilt(error):
         p.ChangeDutyCycle(center + error)
         time.sleep(0.1)
     else:
-        print("Too small to tilt")
+        print("Too small to tilt")"""
 
 
 def rotate_idle():
@@ -210,7 +210,7 @@ def rotate_idle():
     stepper_spin(search_step, direction)
 
 
-def calculate_error(region):
+"""def calculate_error(region):
     # region = [x1,y1,x2,y2]
     error_hor = 20
     # error_vert = 0
@@ -231,17 +231,18 @@ def get_video_output(out=None):
     if out:
         out.release()
     return cv2.VideoWriter('video/test ' + str(time.strftime('%Y-%m-%d_%H.%M.%S_Turret1')) + '.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (frame_width, frame_height))
-    # (YYYY-MM-DD_HR.MIN.SEC_CAMERA_ID)., was '%d_%m_%Y_%H_%M_%S'
+    # (YYYY-MM-DD_HR.MIN.SEC_CAMERA_ID)., was '%d_%m_%Y_%H_%M_%S'"""
 
 
 try:
     init()
-    new_video_out_object_needed = 0
+    #new_video_out_object_needed = 0
     #out = get_video_output()
     count = 0
     #total_time = time.time()
     #start_time = time.time()
     pi.write(enablePIN, 0)  # Enable stepper driver
+    status = 0
 
     while 1:
         print(status, count)
@@ -264,7 +265,7 @@ try:
             rotate_idle()
             new_video_out_object_needed = 1
             print("Rotating idle, looking for target")
-        else:  # Pest has been found, aim at target and record video
+        """else:  # Pest has been found, aim at target and record video
             # error_hor_angle, error_vert_angle = calculate_horizontal_error(region_global)
             # error_vert_angle = 0
             # rotate_to_target(error_hor_angle)
@@ -276,7 +277,7 @@ try:
             #record_video(out)
             print("Target found")
 
-            """total_time = time.time() - start_time
+            total_time = time.time() - start_time
             if total_time >= 30:
                 new_video_out_object_needed = 1
                 start_time = time.time()

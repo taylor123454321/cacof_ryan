6137bc1610ee66d6e72d404d3f2fa0cb7284bb4e
taylor123454321 <ryantaylor.nz@icloud.com>
2022-03-08 19:20:43 +1300

    test new dbus test

--- stepper/rehide_and_seek.py
+++ stepper/rehide_and_seek.py
@@ -2,9 +2,6 @@
 
 # Take in a single optional integral argument
 import sys
-import time
-"""Servo library import"""
-import pigpio
 
 DBUS_NAME = "org.cacophony.thermalrecorder"
 DBUS_PATH = "/org/cacophony/thermalrecorder"
@@ -13,65 +10,57 @@ from gi.repository import GLib
 
 import dbus
 import dbus.mainloop.glib
-
-print("Init GPIO start")
-pi = pigpio.pi()
-"""Init for PIGPIO for stepper driver"""
-stepPIN = 27  # pin 13 on RPI
-dirPIN = 22  # pin 15 on RPI
-enablePIN = 23  # pin 16 on RPI
-pi.set_mode(stepPIN, pigpio.INPUT)
-pi.set_mode(dirPIN, pigpio.INPUT)
-pi.set_mode(enablePIN, pigpio.INPUT)
-
-def handler(sender=None):
-    print("got signal from %r" % sender)
-
+import time
+import threading
 
 def catchall_tracking_signals_handler(what, confidence, region, tracking):
-    if what == 'false-positives':
-        print(
-            "Received a tracking signal and it says " + what,
-            confidence,
-            "% at ",
-            region,
-            " tracking?",
-            tracking,
-        )
-
-
-def hello(steps):
-    bit = 0
-    pi.write(dirPIN, 0)  # CCW
-    for i in range(steps):
-        pi.write(stepPIN, bit)
-        bit = 1 - bit  # Swap value from 1 to 0 to 1 etc
-        time.sleep(0.005)  # 0.001 = 2RPM or 35s cycle time
-    return True
-
-
-if __name__ == "__main__":
-    global object
-    dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
-    loop = GLib.MainLoop()
-    try:
-        bus = dbus.SystemBus()
-        object = bus.get_object(DBUS_NAME, DBUS_PATH)
-    except dbus.exceptions.DBusException as e:
-        print("Failed to initialize D-Bus object: '%s'" % str(e))
-        sys.exit(2)
-
-    bus.add_signal_receiver(
-        catchall_tracking_signals_handler,
-        dbus_interface=DBUS_NAME,
-        signal_name="Tracking",
+    print(
+        "Received a trackng signal and it says " + what,
+        confidence,
+        "% at ",
+        region,
+        " tracking?",
+        tracking,
     )
 
-    pi.write(enablePIN, 0)  # Enable stepper driver
-
-    GLib.timeout_add(100, hello, 50)
-
-    # GLib.timeout_add(1000, make_calls)
 
 
-    loop.run()
+# helper class to run dbus in background
+class TrackingService:
+    def __init__(self,callback):
+        dbus.mainloop.glib.DBusGMainLoop(set_as_default=True)
+        self.callback = callback
+        self.loop = GLib.MainLoop()
+        self.t = threading.Thread(
+            target=self.run_server,
+        )
+        self.t.start()
+
+    def quit(self):
+        self.loop.quit()
+
+    def run_server(self):
+        try:
+            bus = dbus.SystemBus()
+            object = bus.get_object(DBUS_NAME, DBUS_PATH)
+        except dbus.exceptions.DBusException as e:
+            print("Failed to initialize D-Bus object: '%s'" % str(e))
+            sys.exit(2)
+
+        bus.add_signal_receiver(
+            self.callback,
+            dbus_interface=DBUS_NAME,
+            signal_name="Tracking",
+        )
+        self.loop.run()
+
+if __name__ == "__main__":
+    tracking = TrackingService(catchall_tracking_signals_handler)
+
+    # just to keep program alive
+    # replace with your code
+    count = 1
+    while tracking.t.is_alive():
+        print(count)
+        count += 1
+        time.sleep(1)

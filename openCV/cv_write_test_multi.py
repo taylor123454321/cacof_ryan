7b72e52041854984f0963732fed919153cec9c24
taylor123454321 <ryantaylor.nz@icloud.com>
2022-07-30 09:15:30 +1200

    fixed camera prog

--- openCV/cv_write_test_multi.py
+++ openCV/cv_write_test_multi.py
@@ -1,8 +1,10 @@
 import time
-time.sleep(30)
+#time.sleep(30)
 
 import numpy as np
 import cv2
+
+time_flag = False 
 
 cap = cv2.VideoCapture(0)
 frame_width = int(cap.get(3))
@@ -12,22 +14,22 @@ def get_video_output(out=None):
     #Specify the path and name of the video file as well as the encoding, fps and resolution
     if out:
         out.release()
-    #return cv2.VideoWriter('video/static_comp_IR_ ' + str(time.strftime('%Y-%m-%d_%H.%M.%S_Turret1')) + '.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (frame_width, frame_height))
-    return cv2.VideoWriter('../home/pi/Documents/video/static_comp_IR_ ' + str(time.strftime('%Y-%m-%d_%H.%M.%S_Turret1')) + '.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (frame_width, frame_height))
+    return cv2.VideoWriter('video/static_comp_IR_ ' + str(time.strftime('%Y-%m-%d_%H.%M.%S_Turret1')) + '.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (frame_width, frame_height))
+    #return cv2.VideoWriter('../home/pi/Documents/video/static_comp_IR_ ' + str(time.strftime('%Y-%m-%d_%H.%M.%S_Turret1')) + '.avi', cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'), 30, (frame_width, frame_height))
 
 
 
-next_time = time.time() + 120
+next_time = time.time() + 10#20
 out = get_video_output()
 
 try:
     while True:
         hour = int(time.strftime('%H'))
-        print(type(hour))
-        print(hour)
-        if hour > 22 or hour < 2:
+        #print(type(hour))
+        #print(hour)
+        if hour > 18 or hour < 7 or time_flag == True:
             if time.time() > next_time:
-                next_time += 120
+                next_time += 10#20
                 out = get_video_output(out)
                 print("New video")
 
